42f1a7400b4b7a5814db0b7741d3e323c2a92fae
junggyu <jungkyu201@naver.com>
2025-01-03 14:20:25 +0900

    22

--- IntelliJ/bob/src/main/java/fs/four/human/Board/dao/BoardDAO.java
+++ IntelliJ/bob/src/main/java/fs/four/human/Board/dao/BoardDAO.java
@@ -1,4 +1,14 @@
 package fs.four.human.Board.dao;
 
-public class BoardDAO {
+import fs.four.human.Board.vo.BoardVO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface BoardDAO {
+
+    // 전체 게시판 조회
+    List<BoardVO> getAllBoard();
+
 }
--- IntelliJ/bob/src/main/java/fs/four/human/Board/vo/BoardVO.java
+++ IntelliJ/bob/src/main/java/fs/four/human/Board/vo/BoardVO.java
@@ -1,4 +1,25 @@
 package fs.four.human.Board.vo;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
 public class BoardVO {
+    private int B_ID;
+    private String B_CATEGORY;
+    private String B_CC;
+    private String B_TITLE;
+    private String B_CONTENT;
+    private String B_CREATED_ID;
+    private Date B_CREATED_DATE;
+    private String B_UPDATED_ID;
+    private Date B_UPDATED_DATE;
+    private int B_VIEWS;
+
+    public BoardVO() {
+    }
 }
