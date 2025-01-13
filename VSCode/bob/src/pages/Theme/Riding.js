d0b0ed3b2c93fa0950ef361728f884902f5c21fd
kiseokH <rltjrtmsk12@naver.com>
2025-01-13 16:07:12 +0900

    dsa

--- VSCode/bob/src/pages/Theme/Riding.js
+++ VSCode/bob/src/pages/Theme/Riding.js
@@ -11,28 +11,28 @@ const categories = [
         TR_PLACE_NAME: '동해안의 숨은 비경',
         TR_NUMPRODUCE1: '강원 삼척 이사부길',
         TR_NUMPRODUCE2: '신라 장군 이사부를 따서 지은 길입니다.',
-        IMAGE: '이사부길.PNG',
+        IMAGE: 'IsabuRoad.PNG',
       },
       {
         TR_PLACE_ID: 2,
         TR_PLACE_NAME: '어우러짐의 미학',
         TR_NUMPRODUCE1: '제주 신풍해안도로',
         TR_NUMPRODUCE2: '제주의 깊은 바다, 현무암, 풍력발전기와 산뜻한 바람 이 모든 것이 어우러지는 최고의 명소.',
-        IMAGE: '신풍해안도로.PNG',
+        IMAGE: 'SinpoongCoastalRoad.PNG',
       },
       {
         TR_PLACE_ID: 3,
         TR_PLACE_NAME: '노을이 가장 아름다운',
         TR_NUMPRODUCE1: '영광 백수해안도로',
         TR_NUMPRODUCE2: '대한민국 자연경관 대상을 수상한 곳입니다. 환상적인 낙조와 기암괴석으로 이루어진 절벽을 감상하세요.',
-        IMAGE: '영광백수해안도로.PNG',
+        IMAGE: 'YeonggwangBaeksuCoastalRoad.PNG',
       },
       {
         TR_PLACE_ID: 4,
         TR_PLACE_NAME: '이름도 아름다운',
         TR_NUMPRODUCE1: '남해 물미해안도로',
         TR_NUMPRODUCE2: 'S자로 굽이치는 이 길은 남해 최고의 절경이라 할 수 있습니다. 코스 중간 멋진 해변과 방조어부림을 꼭 들러보세요.',
-        IMAGE: '물미해안도로.PNG',
+        IMAGE: 'MulmiCoastalRoad.PNG',
       },
     ],
   },
@@ -44,14 +44,14 @@ const categories = [
         TR_PLACE_NAME: '대통령 공식 별장',
         TR_NUMPRODUCE1: '청주 청남대 가로수길',
         TR_NUMPRODUCE2: '남쪽 청와대라는 뜻을 가지고 있으며 "한국의 아름다운 길 100선"에 포함된 명소입니다.',
-        IMAGE: '청남대.PNG',
+        IMAGE: 'Cheongnamdae.PNG',
       },
       {
         TR_PLACE_ID: 6,
         TR_PLACE_NAME: '역사와 함께하는',
         TR_NUMPRODUCE1: '경기도 광주 남한산성',
         TR_NUMPRODUCE2: '유네스코 세계문화유산으로 지정된 남한산성의 성벽과 어우러진 와일드한 단풍을 느낄 수 있는 명소.',
-        IMAGE: '남한산성.PNG',
+        IMAGE: 'Namhansanseong.PNG',
       },
     ],
   },
@@ -63,14 +63,14 @@ const categories = [
         TR_PLACE_NAME: '낮보다 찬란한 밤',
         TR_NUMPRODUCE1: '대구 83타워',
         TR_NUMPRODUCE2: '대구를 대표하는 타워인 83타워입니다. 달과 별, 그리고 도시 불빛이 어우러진 대구를 감상해보세요.',
-        IMAGE: '83타워.PNG',
+        IMAGE: '83Tower.PNG',
       },
       {
         TR_PLACE_ID: 8,
         TR_PLACE_NAME: '하늘로 향하는 길',
         TR_NUMPRODUCE1: '서울 북악스카이웨이',
         TR_NUMPRODUCE2: '꼬불꼬불 멋진 코스를 오르면 서울 시내가 발아래 펼쳐지는 곳입니다. 아름다운 팔각정과 서울의 야경을 담아보세요.',
-        IMAGE: '북악스카이웨이.PNG',
+        IMAGE: 'BukakSkyway.PNG',
       },
     ],
   },
@@ -113,21 +113,21 @@ const Riding = () => {
         <div className="photo-box-wrapper">
           <div className="photo-box" onClick={() => scrollToSection(0)}>
             <img
-              src="/images/Riding/해안.png"
+              src="/images/Riding/Coastline.png"
               alt="어우러짐의 미학"
               className="photo-box-image"
             />
           </div>
           <div className="photo-box" onClick={() => scrollToSection(1)}>
             <img
-              src="/images/Riding/단풍.png"
+              src="/images/Riding/AutumnLeaves.png"
               alt="노을이 가장 아름다운"
               className="photo-box-image"
             />
           </div>
           <div className="photo-box" onClick={() => scrollToSection(2)}>
             <img
-              src="/images/Riding/야경.png"
+              src="/images/Riding/NightView.png"
               alt="야간"
               className="photo-box-image"
             />
