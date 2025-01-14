cb6bfb51bcc19a989e923211329809adcc5944fa
junggyu <jungkyu201@naver.com>
2025-01-14 12:21:40 +0900

    back

--- IntelliJ/bob/src/main/java/fs/four/human/MyPage/dao/MyPageDAO.java
+++ IntelliJ/bob/src/main/java/fs/four/human/MyPage/dao/MyPageDAO.java
@@ -5,5 +5,10 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface MyPageDAO {
+
+    // 기존 조회
     MyPageVO getUserInfo(String uId);
+
+//    // 사용자 저보 수정
+//    int updateUserInfo(MyPageVO userInfo);
 }
