ac1ce432d09ff935df6056e1ce2ce570e0cc314a
kiseokH <rltjrtmsk12@naver.com>
2025-01-15 12:36:38 +0900

    tqtqtqTLqkfsusdk

--- IntelliJ/bob/src/main/java/fs/four/human/Login/Controller/SignupController.java
+++ IntelliJ/bob/src/main/java/fs/four/human/Login/Controller/SignupController.java
@@ -12,13 +12,13 @@ public class SignupController {
     @Autowired
     private SignupService signupService;
 
-    // 아이디 중복 체크 API - POST 요청으로 변경
-    @PostMapping("/check-id")
+    // 아이디 중복 체크 (GET, POST 모두 허용)
+    @RequestMapping(value = "/check-id", method = {RequestMethod.GET, RequestMethod.POST})
     public boolean checkIdDuplicate(@RequestParam String uId) {
         return signupService.checkIdDuplicate(uId);
     }
 
-    // 회원가입 API
+    // 회원가입 (POST 요청만 허용)
     @PostMapping("/register")
     public String registerUser(@RequestBody SignupVO signupVO) {
         try {
