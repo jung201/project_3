fd395122a3ed7e0c3646d69bb905e55c4160af35
junggyu <jungkyu201@naver.com>
2025-01-17 12:00:05 +0900

    123123

--- IntelliJ/bob/src/main/java/fs/four/human/BobApplication.java
+++ IntelliJ/bob/src/main/java/fs/four/human/BobApplication.java
@@ -2,12 +2,13 @@ package fs.four.human;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BobApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BobApplication.class, args);
 	}
-
 }
